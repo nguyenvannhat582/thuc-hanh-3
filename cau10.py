a7406318f5f03fbc91f21d15d06e49db2f2d93df
nguyenvannhat582 <48303068+nguyenvannhat582@users.noreply.github.com>
2019-05-01 16:11:57 +0700

    th3

---
+++ cau10.py
@@ -0,0 +1,6 @@
+import math
+r = float(input("Nhap vao ban kinh hinh tron ---> "))
+chuvi = 2*math.pi*r
+dientich = math.pi*r*r
+print("Chu vi hinh tron co ban kinh {0} la ---> {1} ".format(r,chuvi))
+print("Dien tich hinh tron co ban kinh {0} la ---> {1} ".format(r,dientich))
